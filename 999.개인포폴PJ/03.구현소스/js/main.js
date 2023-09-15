2bbd54506a5abc25852407378932ec7fa696121f
Tom B. Parker <tombap8@hotmail.com>
2023-09-15 17:47:04 +0900

    탐쌤

--- 999.개인포폴PJ/03.구현소스/js/main.js
+++ 999.개인포폴PJ/03.구현소스/js/main.js
@@ -1,1 +1,27 @@
 ////////////////// 포폴 JS 영역 ////////////////////////////
+
+// DOM 함수 객체 //////////////
+const domFn = {
+    // 요소선택함수 ////////
+    qs: (x) => document.querySelector(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsa: (x) => document.querySelectorAll(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+  
+    // 이벤트셋팅함수
+    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+  }; /////// domFn 객체 /////////////
+  
+
+// 대상: .btn-grp button
+const btns = domFn.qsa('.btn-grp button');
+const mapImg = domFn.qs('.map img');
+
+btns.forEach((ele,idx)=>{
+
+    domFn.addEvt(ele,'click',(e)=>{
+        e.preventDefault();
+        // alert(idx);
+        mapImg.src = `../03.구현소스/images/map/map${idx+1}.jpg`;
+    })
+})
